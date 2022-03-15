45a6f21782427ca9cb90d7a7dca68d510b16d972
Labib <lstriker1268@gmail.com>
2022-03-15 19:17:06 +0000

    added error messages

--- index.js
+++ index.js
@@ -3,7 +3,19 @@ import prompts from "prompts"
 const response = await prompts({
     type: 'text',
     name: 'wordle',
-    message: 'What word would you like to input?'
+    message: 'What word would you like to input?',
+    validate: function(input) {
+        let length = input.length
+        if (input.length == 5) {
+            return true
+        }
+
+        if (input.length > 5) {
+            return "This word is longer than 5 characters!"
+        }
+        
+        return "This word is shorter than 5 characters!"
+    }
 });
 
 console.log(response.wordle)
